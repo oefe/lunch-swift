2c2748bb17bbf9ca41a0fe8b1b8b54330f3349f4
Martina Oefelein <Martina@Oefelein.de>
2015-06-19 22:08:27 +0200

    Added Json serialization

--- Lunch/Order.swift
+++ Lunch/Order.swift
@@ -12,4 +12,47 @@ class Order {
     var current = [false, false, false, false, false]
     var next = [false, false, false, false, false]
     var alreadyOrdered = false
+    let today: NSDate
+    
+    init(today: NSDate) {
+        self.today = today
+    }
+    
+    init(today: NSDate, withJsonObject json: NSDictionary) {
+        self.today = today
+        if let thisWeek = json[jsonKey(0)] as? NSDictionary {
+            current = thisWeek[Order.ordersJsonKey] as! [Bool]
+        }
+        if let nextWeek = json[jsonKey(1)] as? NSDictionary {
+            alreadyOrdered = nextWeek[Order.finalJsonKey] as! Bool
+            next = nextWeek[Order.ordersJsonKey] as! [Bool]
+        }
+    }
+    
+    func asJsonObject() -> NSDictionary {
+        return [
+            jsonKey(0): [
+                Order.finalJsonKey: true,
+                Order.ordersJsonKey: current
+            ],
+            jsonKey(1): [
+                Order.finalJsonKey: alreadyOrdered,
+                Order.ordersJsonKey: next
+            ]
+        ]
+    }
+    
+    // Constants
+    private static let oneDay = 24 * 60 * 60.0
+    private static let oneWeek = 7 * oneDay
+    private static let finalJsonKey = "final"
+    private static let ordersJsonKey = "orders"
+    
+    // Helper functions
+    private func jsonKey(weeksForward: Double) -> String{
+        let date = today.dateByAddingTimeInterval(weeksForward * Order.oneWeek)
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "YYYY-ww"
+        return formatter.stringFromDate(date)
+    }
 }
--- LunchTests/LunchTests.swift
+++ LunchTests/LunchTests.swift
@@ -22,12 +22,67 @@ class LunchTests: XCTestCase {
     }
     
     func testEmpty() {
-        let order = Order()
+        let order = Order(today: date("17.06.2015"))
         
         XCTAssertEqual(order.alreadyOrdered, false, "not ordered yet")
         XCTAssertEqual(order.current, [false, false, false, false, false], "nothing ordered this week")
         XCTAssertEqual(order.next, [false, false, false, false, false], "nothing ordered next week")
-     }
+    }
+    
+    func testSaveLoadSameDay() {
+        let old = Order(today: date("24.06.2015"))
+        old.alreadyOrdered = true
+        old.current[1] = true
+        old.next[2] = true
+        let json = old.asJsonObject()
+        
+        let new = Order(today: date("24.06.2015"), withJsonObject: json)
+        
+        XCTAssertEqual(new.alreadyOrdered, true, "already ordered")
+        XCTAssertEqual(new.current, [false, true, false, false, false], "Tuesday ordered this week")
+        XCTAssertEqual(new.next, [false, false, true, false, false], "Wednesday ordered next week")
+    }
+    
+    func testSaveLoadSameWeek() {
+        let old = Order(today: date("24.06.2015"))
+        old.alreadyOrdered = false
+        old.current[3] = true
+        old.next[4] = true
+        let json = old.asJsonObject()
+        
+        let new = Order(today: date("25.06.2015"), withJsonObject: json)
+        
+        XCTAssertEqual(new.alreadyOrdered, false, "not already ordered")
+        XCTAssertEqual(new.current, [false, false, false, true, false], "Thursday ordered this week")
+        XCTAssertEqual(new.next, [false, false, false, false, true], "Friday ordered next week")
+    }
+    
+    func testSaveLoadNextWeek() {
+        let old = Order(today: date("24.06.2015"))
+        old.alreadyOrdered = true
+        old.current[1] = true
+        old.next[0] = true
+        let json = old.asJsonObject()
+        
+        let new = Order(today: date("29.06.2015"), withJsonObject: json)
+        
+        XCTAssertEqual(new.alreadyOrdered, false, "not already ordered")
+        XCTAssertEqual(new.current, [true, false, false, false, false], "Monday ordered this week")
+    }
+    
+    func testSaveLoadTwoWeeksLater() {
+        let old = Order(today: date("24.06.2015"))
+        old.alreadyOrdered = true
+        old.current[1] = true
+        old.next[2] = true
+        let json = old.asJsonObject()
+        
+        let new = Order(today: date("06.07.2015"), withJsonObject: json)
+        
+        XCTAssertEqual(new.alreadyOrdered, false, "not ordered yet")
+        XCTAssertEqual(new.current, [false, false, false, false, false], "nothing ordered this week")
+        XCTAssertEqual(new.next, [false, false, false, false, false], "nothing ordered next week")
+    }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
@@ -36,4 +91,10 @@ class LunchTests: XCTestCase {
         }
     }
     
+    private func date(when: String) -> NSDate {
+        let formatter = NSDateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "de")
+        formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        return formatter.dateFromString(when)!
+    }
 }
